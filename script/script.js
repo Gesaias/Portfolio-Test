40b810c8cf160c7f76e08b6f6bb2f5d25e7e1992
Gesaias <alvesgesaias@gmail.com>
2021-12-23 11:51:50 -0400

    Conhecimentos

--- script/script.js
+++ script/script.js
@@ -1,11 +1,3 @@
 document.querySelector(".hamburguer").addEventListener("click", () => {
-    document.querySelector(".hamburguer").classList.toggle("showMenu1");
-})
-
-document.querySelector(".hamburguer").addEventListener("click", () => {
-    document.querySelector(".sidebar").classList.toggle("showMenu2");
-})
-
-document.querySelector(".hamburguer").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("showMenuL");
+    document.querySelector(".container").classList.toggle("showMenu");
 })
